575d849ff730a13be008500977f1ad4001b70dbd
shashwatssp <88626771+shashwatssp@users.noreply.github.com>
2022-05-31 00:10:58 +0530

    Time: 22 ms (32.11%), Space: 15.9 MB (13.30%) - LeetHub

--- 229-majority-element-ii/229-majority-element-ii.cpp
+++ 229-majority-element-ii/229-majority-element-ii.cpp
@@ -1,23 +1,46 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        map<int,int> map;
-        vector<int> ans;
+  vector<int> ans;
+        int c1=INT_MIN;
+        int c2=INT_MIN;
+        int v1=0;
+        int v2=0;
         
         for(int i=0;i<nums.size();i++)
         {
-            map[nums[i]]++;
-        }
-        
-        for(auto it : map)
-        {
-            if(it.second> (nums.size())/3)
+            if(nums[i]==c1)
+                v1++;
+            else if(nums[i]==c2)
+                v2++;
+            else if(v1==0)
+                c1=nums[i], v1=1;
+            else if(v2==0)
+                c2=nums[i], v2=1;
+            else
             {
-                ans.push_back(it.first);
-             }
+                v1--;
+                v2--;
+            }
         }
         
-        return ans;
+       v1=0;
+       v2=0;
         
+       for(int i=0;i<nums.size();i++)
+       {
+       if(nums[i]==c1)
+           v1++;
+       if(nums[i]==c2)
+           v2++;
+       }
+        
+        if(v1>nums.size()/3)
+            ans.push_back(c1);
+        if(v2>nums.size()/3)
+            ans.push_back(c2);
+        
+    
+        return ans;
     }
 };
