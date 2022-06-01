0cb80b086bf9ba7f6636f8f24232eb111da4896b
shashwatssp <88626771+shashwatssp@users.noreply.github.com>
2022-06-02 00:29:39 +0530

    Time: 235 ms (49.91%), Space: 68.8 MB (32.96%) - LeetHub

---
+++ 997-find-the-town-judge/997-find-the-town-judge.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int findJudge(int n, vector<vector<int>>& trust) {
+        vector<int>score(n+1,0);
+        for(auto x : trust)
+        {
+            score[x[0]]--;
+            score[x[1]]++;
+        }
+        
+      for(int i=1;i<score.size();i++)
+      {
+       if(score[i]==(n-1))
+       {
+           return i;
+       }
+      }
+        
+        return -1;
+     }
+                
+        
+};
