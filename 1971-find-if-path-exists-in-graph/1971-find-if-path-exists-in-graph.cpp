86b82332bffddad37936f9cbf7131c910edb4e05
shashwatssp <88626771+shashwatssp@users.noreply.github.com>
2022-06-01 23:40:12 +0530

    Time: 1369 ms (12.42%), Space: 208.6 MB (29.68%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.cpp
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.cpp
@@ -1,36 +1,40 @@
 class Solution {
 public:
     bool validPath(int n, vector<vector<int>>& edges, int source, int destination) {
-        unordered_map<int,vector<int>> umap;
+        unordered_map<int,vector<int>>umap;
         for(auto x : edges)
         {
-            vector<int>temp = x;
-        int a = temp[0];
-        int b = temp[1];
+            vector<int>test=x;
+            
+            int a = test[0];
+            int b = test[1];
+            
             umap[a].push_back(b);
             umap[b].push_back(a);
-        
         }
         
         queue<int> q;
+        q.push(source);
+        
         vector<bool>visited(n+1,false);
         visited[source]=true;
-        q.push(source);
         
         while(!q.empty())
         {
-            
-        int f = q.front();
+            int f = q.front();
             q.pop();
             
             for(auto x : umap[f])
             {
-               if(visited[x]==false)
-               {
-               visited[x]=true;
-                   q.push(x);
-               }
+                if(visited[x]==false)
+                {
+                    visited[x]=true;
+                    q.push(x);
+                }
             }
+            
+            if(visited[destination])
+                return visited[destination];
         }
         
         return visited[destination];
