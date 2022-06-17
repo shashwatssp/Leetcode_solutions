dfeb461b1be6fca5334b4f94bf481793d31dd34c
shashwatssp <88626771+shashwatssp@users.noreply.github.com>
2022-06-18 00:43:44 +0530

    Time: 4 ms (30.77%), Space: 6.4 MB (50.89%) - LeetHub

---
+++ 20-valid-parentheses/20-valid-parentheses.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char>st;
+        for(int i=0;i<s.size();i++)
+        {
+        if(s[i]=='(' || s[i]=='[' || s[i]=='{')
+        {
+            st.push(s[i]);
+        }
+            else if (s[i]==')' && ( i==0 || st.empty() || st.top()!= '('))
+            {
+             return false;
+            }  
+            else if (s[i]=='}' && ( i==0 || st.empty() || st.top()!= '{'))
+            {
+            return false;
+            st.pop();
+            }
+          else if (s[i]==']' && (i==0 || st.empty() || st.top()!= '['))
+            {
+            return false;
+             st.pop();
+             }
+            
+            else
+            {
+          st.pop();
+            }
+                
+
+        }
+        
+        if(st.empty())
+            return true;
+        else
+            return false;
+    }
+};
