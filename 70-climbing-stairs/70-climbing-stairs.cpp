78ca7d6ec7f2c657ba0b734a337c4070bdb39bd7
shashwatssp <88626771+shashwatssp@users.noreply.github.com>
2022-07-06 21:56:32 +0530

    Time: 4 ms (22.49%), Space: 6.3 MB (14.97%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -1,16 +1,22 @@
 class Solution {
 public:
-    int climbStairs(int n) {
-        int prev = 1, cur = 2;
-        if(n==1)
-            return prev;
-        if(n==2)
-            return cur;
-        for(int i = 2; i < n; i++){
-            int next = cur;
-            cur += prev;
-            prev = next;
-        }
-        return cur;
+    int fib(int n ,vector<int>&dp)
+    {
+        if(n<=1) return dp[n]=1;
+        
+        
+        if(dp[n]!=-1) return dp[n];
+        
+        dp[n] = fib(n-1,dp) + fib(n-2,dp);
+        
+        return dp[n];
+
     }
+    vector<int>dp = vector<int>(100,-1);
+    int climbStairs(int n) {
+        vector<int> dp(n+1,-1);
+        return fib(n,dp);
+    }
+              
+
 };
