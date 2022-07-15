d6f415cc043049d067d6a924176fd9559188f99c
shashwatssp <88626771+shashwatssp@users.noreply.github.com>
2022-07-15 16:52:37 +0530

    Time: 78 ms (25.84%), Space: 20.5 MB (60.83%) - LeetHub

---
+++ 49-group-anagrams/49-group-anagrams.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        
+        vector<vector<string>> ans;
+        
+        unordered_map<string, vector<string>> map;
+        
+        for(int i=0;i<strs.size();i++)
+        {
+         string temp = strs[i];
+         sort(strs[i].begin(), strs[i].end());
+         map[strs[i]].push_back(temp);
+        }
+        
+        for(auto it : map)
+        ans.push_back(it.second);
+        
+        return ans;
+        
+    }
+};
