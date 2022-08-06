ab7723daabc415ca48588ca483d0ec612d183b37
shashwatssp <88626771+shashwatssp@users.noreply.github.com>
2022-08-06 14:26:56 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (93.01%) - LeetHub

--- 43-multiply-strings/43-multiply-strings.cpp
+++ 43-multiply-strings/43-multiply-strings.cpp
@@ -1,20 +1,21 @@
 class Solution {
 public:
     string multiply(string num1, string num2) {
-    string ans(num1.size()+num2.size(),'0');
-    for(int i=num1.size()-1; i>=0; i--)
-    {   int carry = 0;
-        for(int j=num2.size()-1;j>=0;j--)
+    string ans(num1.size() + num2.size(), '0');
+    for(int i = num1.size()-1; i>=0; i--)
+    {
+        int carry = 0;
+        for(int j = num2.size()-1; j>=0;j--)
         {
-          int temp = (ans[i+j+1]-'0') + ((num1[i]-'0')*(num2[j]-'0')) + carry;
-          ans[i+j+1] = (temp%10) + '0';
-          carry = temp/10;
+            int temp = (ans[i+j+1]-'0') + ((num1[i]-'0')*(num2[j]-'0')) + carry;
+            ans[i+j+1] = temp%10 + '0';
+            carry=temp/10;
         }
-       ans[i]+= carry;
+        ans[i]+=carry;
     }
         
-    int pos=0;
-    while(pos<ans.size() && ans[pos]=='0') pos++;
+        int pos = 0;
+        while(pos<ans.size() && ans[pos]=='0') pos++;
         if(pos==ans.size()) return "0";
         
         return ans.substr(pos);
