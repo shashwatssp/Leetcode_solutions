7a1f350aa8e8a9a77fac444ff8cda6ec82c8fc13
Shashwat Shagun Pandey <88626771+shashwatssp@users.noreply.github.com>
2023-04-01 02:27:26 +0530

    Time: 66 ms (27.37%), Space: 15.2 MB (77.61%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
-    int dp[1001][1001];
-    int solve(string s, string t, int i, int j)
-    {
-        if(j<0) return 1;
-        if(i<0) return 0;
-        
-        // cout<<i<<" "<<j<<endl;
-        if(dp[i][j]!=-1) return dp[i][j];
-        
-        if(s[i]==t[j]) return dp[i][j] = solve(s,t,i-1,j-1) + solve(s,t,i-1,j);
-        else return dp[i][j] = solve(s,t,i-1,j);
+   double dp[1001][1001];  
 
-    }
     int numDistinct(string s, string t) {
         memset(dp,-1,sizeof(dp));
-        return solve(s,t,s.size()-1,t.size()-1);
+        
+        int n = s.size();
+        int m = t.size();
+        
+        for(int i=0;i<=n;i++) dp[i][0] = 1;
+        for(int j=1;j<=m;j++) dp[0][j] = 0;
+        
+        for(int i=1;i<=n;i++)
+            for(int j=1;j<=m;j++)
+                dp[i][j] = (s[i-1]==t[j-1])?dp[i-1][j-1] + dp[i-1][j]:dp[i-1][j];
+        
+        return (int) dp[n][m];
     }
 };
