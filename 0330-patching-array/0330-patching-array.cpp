1c4fee7f8dd7655956e66732c60ee53fde2eb7c0
Shashwat Shagun Pandey <88626771+shashwatssp@users.noreply.github.com>
2023-04-01 07:16:18 +0530

    Time: 8 ms (54.89%), Space: 11.4 MB (8.70%) - LeetHub

--- 0330-patching-array/0330-patching-array.cpp
+++ 0330-patching-array/0330-patching-array.cpp
@@ -2,10 +2,10 @@ class Solution {
 public:
     int minPatches(vector<int>& nums, int n) {
         long long reach=0,i=0,ans=0;
-        
+        int sz = nums.size();
         while(reach<n)
         {   
-            if(i<nums.size() && reach+1>=nums[i])
+            if(i<sz && reach+1>=nums[i])
             reach+=nums[i++];
             else 
             {
