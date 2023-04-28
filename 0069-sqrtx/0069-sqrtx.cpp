077b6331928a8af420ae29cb01583367a1c86bb1
Shashwat Shagun Pandey <88626771+shashwatssp@users.noreply.github.com>
2023-04-28 22:31:21 +0530

    Time: 3 ms (65.47%), Space: 5.9 MB (62.84%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -5,6 +5,7 @@ public:
         long long st=1;
         long long en=x;
         long long ans = 0;
+        long long mid = (st+en+1)/2; 
         while(st<=en)
         {
          long long mid = (st+en+1)/2; 
@@ -19,9 +20,8 @@ public:
             else
                 en = mid-1;
             
+           mid = (st+en+1)/2; 
         }
-        return ans;
-        
-        
+       return ans;
     }
 };
