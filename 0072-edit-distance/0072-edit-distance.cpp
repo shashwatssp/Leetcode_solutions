c5de601825df961e687d1d53c5cf9dc193f86a8c
Shashwat Shagun Pandey <88626771+shashwatssp@users.noreply.github.com>
2023-07-31 21:03:46 +0530

    Time: 47 ms (23.86%), Space: 7.4 MB (81.64%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -3,18 +3,27 @@ public:
     int dp[510][510];
     int n=0;
     int m=0;
-    int solve(string word1, string word2, int i, int j)
-    {   
-        if(i<0) return j+1;
-        if(j<0) return i+1;
-        if(dp[i][j]!=-1) return dp[i][j];
-        if(word1[i]==word2[j]) return dp[i][j] = solve(word1,word2,i-1,j-1);
-        return dp[i][j] = 1 + min(min(solve(word1,word2,i-1,j),solve(word1,word2,i,j-1)),solve(word1,word2,i-1,j-1));
+    int solve(string word1, string word2, int n, int m)
+    {  
+    dp[0][0]=0;
+        
+    for(int i=1;i<=n;i++)dp[i][0]=i;
+    for(int j=1;j<=m;j++)dp[0][j]=j;
+    
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=m;j++)
+            {
+                if(word1[i-1]==word2[j-1]) dp[i][j] =dp[i-1][j-1];
+                else dp[i][j] = 1+ min(min(dp[i][j-1],dp[i-1][j]),dp[i-1][j-1]);
+            }
+        }
+        return dp[n][m];
     }
     int minDistance(string word1, string word2) {
          memset(dp,-1,sizeof(dp));
          n= word1.size();
          m= word2.size();
-         return solve(word1,word2,n-1,m-1); 
+         return solve(word1,word2,n,m); 
     }
 };
