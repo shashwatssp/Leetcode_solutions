124d1df46bb172e2c15507e0fd09ee4ddaadd0d5
Shashwat Shagun Pandey <88626771+shashwatssp@users.noreply.github.com>
2023-09-04 12:31:06 +0530

    Time: 0 ms (100.00%), Space: 10.3 MB (87.30%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,16 +1,18 @@
 class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
-        vector<int>ans;
+      vector<int> ans;
+      
         for(auto it : nums)
         {
-            if(ans.size()==0 || ans.back()<it) ans.push_back(it);
-            else 
+            if(ans.size()==0 || ans.back()<it)
+                ans.push_back(it);
+            else
             {
                 int lb = lower_bound(ans.begin(),ans.end(),it)-ans.begin();
-                ans[lb] = it;
+                ans[lb]=it;
             }
         }
-        return ans.size();
+    return ans.size();
     }
 };
