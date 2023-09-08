ed86e35ed39c67675aaa786e25090a9dbef55fc5
Shashwat Shagun Pandey <88626771+shashwatssp@users.noreply.github.com>
2023-09-08 19:55:14 +0530

    Time: 168 ms (89.90%), Space: 126.5 MB (95.34%) - LeetHub

--- 1406-stone-game-iii/1406-stone-game-iii.cpp
+++ 1406-stone-game-iii/1406-stone-game-iii.cpp
@@ -1,27 +1,24 @@
 class Solution {
 public:
-    int dp[100000];
+    int dp[3];
     int n;
     int solve(vector<int>& stoneValue)
     {   
-        dp[n]=0;
-        dp[n+1]=0;
-        dp[n+2]=0;
         for(int i=n-1;i>=0;i--)
         {
         int a = INT_MIN;
         int b = INT_MIN;
         int c = INT_MIN;
         
-        a = stoneValue[i] - dp[i+1];
+        a = stoneValue[i] - dp[(i+1)%3];
         
         if(i<n-1)
-        b = stoneValue[i]+stoneValue[i+1] - dp[i+2];
+        b = stoneValue[i]+stoneValue[i+1] - dp[(i+2)%3];
         
         if(i<n-2)
-        c = stoneValue[i] + stoneValue[i+1] + stoneValue[i+2] - dp[i+3];
+        c = stoneValue[i] + stoneValue[i+1] + stoneValue[i+2] - dp[(i+3)%3];
         
-         dp[i] = max(a,max(b,c));
+         dp[i%3] = max(a,max(b,c));
         }
      return dp[0];
     }
