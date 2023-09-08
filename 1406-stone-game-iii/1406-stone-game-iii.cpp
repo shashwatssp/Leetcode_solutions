eb0fd70577fbf3f862acbd1c8f809c5a94e06345
Shashwat Shagun Pandey <88626771+shashwatssp@users.noreply.github.com>
2023-09-08 19:54:01 +0530

    Time: 156 ms (95.34%), Space: 126.8 MB (93.08%) - LeetHub

--- 1406-stone-game-iii/1406-stone-game-iii.cpp
+++ 1406-stone-game-iii/1406-stone-game-iii.cpp
@@ -2,31 +2,32 @@ class Solution {
 public:
     int dp[100000];
     int n;
-    int solve(int i, vector<int>& stoneValue)
+    int solve(vector<int>& stoneValue)
     {   
-        if(i==n) return 0;
-        
-        if(dp[i]!=-1) return dp[i];
-        
+        dp[n]=0;
+        dp[n+1]=0;
+        dp[n+2]=0;
+        for(int i=n-1;i>=0;i--)
+        {
         int a = INT_MIN;
         int b = INT_MIN;
         int c = INT_MIN;
         
-        a = stoneValue[i] - solve(i+1,stoneValue);
+        a = stoneValue[i] - dp[i+1];
         
         if(i<n-1)
-        b = stoneValue[i]+stoneValue[i+1] - solve(i+2,stoneValue);
+        b = stoneValue[i]+stoneValue[i+1] - dp[i+2];
         
         if(i<n-2)
-        c = stoneValue[i] + stoneValue[i+1] + stoneValue[i+2] - solve(i+3, stoneValue);
+        c = stoneValue[i] + stoneValue[i+1] + stoneValue[i+2] - dp[i+3];
         
-        return dp[i] = max(a,max(b,c));
+         dp[i] = max(a,max(b,c));
+        }
+     return dp[0];
     }
     string stoneGameIII(vector<int>& stoneValue) {
         n = stoneValue.size();
-        memset(dp,-1,sizeof(dp));
-        int ans = solve(0,stoneValue);
-        
+        int ans = solve(stoneValue);
         
         if(ans>0)
             return "Alice";
