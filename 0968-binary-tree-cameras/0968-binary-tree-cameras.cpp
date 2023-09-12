c777b93c33e0f60f76d864595efe7bd104b07875
Shashwat Shagun Pandey <88626771+shashwatssp@users.noreply.github.com>
2023-09-12 20:01:35 +0530

    Time: 12 ms (36.25%), Space: 23.7 MB (21.79%) - LeetHub

--- 0968-binary-tree-cameras/0968-binary-tree-cameras.cpp
+++ 0968-binary-tree-cameras/0968-binary-tree-cameras.cpp
@@ -12,38 +12,30 @@
 class Solution {
 public:
     int ans=0;
-    int has_camera = 0;
-    int needs_camera = -1;
-    int covered = 1;
-    
     map<TreeNode*,int>dp;
-    
-    int solve(TreeNode* root)
+    int solve (TreeNode* root)
     {
-        if(root==nullptr)
-            return covered;
+        if(root==nullptr) return dp[root]=15;
         
         if(dp[root]) return dp[root];
         
         int left = solve(root->left);
         int right = solve(root->right);
         
-        if(left==needs_camera || right == needs_camera)
+        if(left==5 || right==5)
         {
             ans++;
-            return dp[root] =  has_camera;
+            return dp[root] = 10;
         }
-        else if (left == has_camera || right == has_camera)
-        {
-            return dp[root]=covered;
-        }
-        else
-        {
-            return dp[root] = needs_camera;
-        }
+        else if(left==10 || right==10)
+            return dp[root]=15;
+
+        else 
+            return dp[root] = 5;
     }
     int minCameraCover(TreeNode* root) {
-        if(solve(root)==needs_camera) ans++;
+        solve(root);
+        if(dp[root]==5) ans++;
         return ans;
     }
 };
