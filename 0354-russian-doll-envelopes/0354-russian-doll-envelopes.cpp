e3c0d0fb2173eb3298f6a7d286105566622b6e77
Shashwat Shagun Pandey <88626771+shashwatssp@users.noreply.github.com>
2023-09-19 00:00:17 +0530

    Time: 303 ms (45.29%), Space: 78 MB (37.63%) - LeetHub

--- 0354-russian-doll-envelopes/0354-russian-doll-envelopes.cpp
+++ 0354-russian-doll-envelopes/0354-russian-doll-envelopes.cpp
@@ -1,31 +1,23 @@
 class Solution {
 public:
-    static bool comp(const vector<int> &p1, vector<int> &p2)
+    static bool comp(vector<int> &p1, vector<int>& p2)
     {
-        if(p1[0]==p2[0]) return p2[1]<p1[1];
-        else
+        if(p1[0]==p2[0]) return p1[1]>p2[1];
         return p1[0]<p2[0];
-
     }
     int maxEnvelopes(vector<vector<int>>& envelopes) {
-       sort(envelopes.begin(),envelopes.end(),comp);
-       vector<int>ans;
-       
-        for(int i=0;i<envelopes.size();i++)
+        sort(envelopes.begin(),envelopes.end(),comp);
+        int n = envelopes.size();
+        vector<int>ans;
+        
+        for(int i=0;i<n;i++)
         {
-            
-            if(ans.size()==0)
-            ans.push_back(envelopes[i][1]);
-            else
-            {   
-                if(envelopes[i][1]>ans.back())
+            if(ans.size()==0 || envelopes[i][1]>ans.back())
                 ans.push_back(envelopes[i][1]);
-                else
-                {
-                  int lb = lower_bound(ans.begin(),ans.end(),envelopes[i][1])-ans.begin();
-                  ans[lb] = envelopes[i][1];   
-                }
-                
+            else
+            {
+                int lb = lower_bound(ans.begin(),ans.end(),envelopes[i][1])-ans.begin();
+                ans[lb] = envelopes[i][1];
             }
         }
         
