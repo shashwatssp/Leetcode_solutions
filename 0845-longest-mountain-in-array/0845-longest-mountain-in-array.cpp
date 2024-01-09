eda69fa33f44fbfb8c10d06259f9e0ee4045f368
Shashwat Shagun Pandey <88626771+shashwatssp@users.noreply.github.com>
2024-01-09 20:18:51 +0530

    Time: 23 ms (18.54%), Space: 19 MB (33.99%) - LeetHub

--- 0845-longest-mountain-in-array/0845-longest-mountain-in-array.cpp
+++ 0845-longest-mountain-in-array/0845-longest-mountain-in-array.cpp
@@ -7,35 +7,22 @@ public:
         int next[n];
         
         for(int i=0;i<n;i++)
-        {
-            prev[i]=1;
-            next[i]=1;
-        }
+            prev[i]=1,next[i]=1;
         
         for(int i=1;i<n;i++)
-        {
             if(arr[i]>arr[i-1])
                 prev[i]+=prev[i-1];
-        }
-        
+      
         
         for(int i=n-2;i>=0;i--)
-        {
             if(arr[i]>arr[i+1])
                 next[i]+=next[i+1];
-        }
-        
-        
-        
+
         int ans=0;
         
         for(int i=1;i<n-1;i++)
-        {
             if(arr[i-1]<arr[i] && arr[i]>arr[i+1])
-            {
              ans = max(ans, prev[i]+next[i]-1);
-            }
-        }
         
     
         return ans;
