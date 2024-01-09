df895885bffc9d6eb9c330f80e3eb54e59dc2e8e
Shashwat Shagun Pandey <88626771+shashwatssp@users.noreply.github.com>
2024-01-09 19:30:33 +0530

    Time: 4 ms (57.02%), Space: 6.9 MB (87.21%) - LeetHub

--- 0763-partition-labels/0763-partition-labels.cpp
+++ 0763-partition-labels/0763-partition-labels.cpp
@@ -1,33 +1,30 @@
 class Solution {
 public:
     vector<int> partitionLabels(string s) {
+        vector<int>v(26,0);
         int n = s.size();
-        map<char,int>second;
         
-            for(int i=n-1;i>=0;i--)
+        for(int i=0;i<n;i++)
+        v[s[i]-'a'] = i;
+        
+        vector<int>ans;
+        
+        int mx=-1;
+        
+        for(int i=0;i<n;i++)
         {
-            if(second[s[i]]) continue;
-            second[s[i]]=i;
-        }
-        
-     int mx=-1;
-     int prev=0;
-     vector<int>ans;
-     
-        for(int i=0;i<s.size();i++)
-        {   
-            mx = max(mx, second[s[i]]);
+            mx = max(mx, v[s[i]-'a']);
+            
             if(mx==i)
-            {   
                 ans.push_back(i+1);
-            }
         }
         
-        for(int i=ans.size()-1;i>0;i--)
-        {
+        n = ans.size();
+        
+        for(int i=n-1;i>0;i--)
             ans[i]-=ans[i-1];
-        }
         
-     return ans;
+        return ans;
+
     }
 };
