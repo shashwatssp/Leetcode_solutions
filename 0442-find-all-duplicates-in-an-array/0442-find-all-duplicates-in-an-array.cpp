b067ce6080fb3108c9aa674c72ce30aefab3e126
Shashwat Shagun Pandey <88626771+shashwatssp@users.noreply.github.com>
2024-01-16 16:14:30 +0530

    Time: 45 ms (74.84%), Space: 33.9 MB (86.98%) - LeetHub

--- 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.cpp
+++ 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.cpp
@@ -4,21 +4,19 @@ public:
         vector<int>ans;
         int n = nums.size();
         
-        int i=0;
-        
-        while(i<n)
+        for(int i=0;i<n;i++)
         {
-            int pos = nums[i]-1;
+            int x = nums[i]-1;
             
-            if(nums[i]!=nums[pos])
-                swap(nums[i],nums[pos]);
-            else
-                i++;
+            if(nums[i]!=nums[x])
+                swap(nums[i--],nums[x]);
         }
         
         for(int i=0;i<n;i++)
-            if(nums[i]!=i+1)
-                ans.push_back(nums[i]);
+        {
+        if(nums[i]!=i+1) ans.push_back(nums[i]);
+        }
+        
         
         return ans;
     }
